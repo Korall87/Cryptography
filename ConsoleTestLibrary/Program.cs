d5feb4f006494f900d09798b4a513d0f066ad4ff
Kirill Kovalev <kirillkovalevv06@gmail.com>
2024-03-09 14:53:02 +0300

    реализовал шифр порта

--- ConsoleTestLibrary/Program.cs
+++ ConsoleTestLibrary/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Xml.Schema;
+using CryptographyLibrary;
 using CryptographyLibrary.CaesarCipher;
 
 namespace ConsoleTestLibrary
@@ -8,7 +9,12 @@ namespace ConsoleTestLibrary
     {
         static void Main(string[] args)
         {
-            
+            var portCipher = new PortCipher();
+
+            portCipher.method("НЕТ", "КЛЮЧ");
+            portCipher.method("нет", "ключ");
+
+
             Console.ReadLine();
         }
     }
---
+++ CryptographyLibrary/PortCipher.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptographyLibrary
+{
+    public class PortCipher
+    {
+        private int CharacterReplacement(int ch, int keyChar)
+        {
+            int Key = keyChar/2;
+
+            if (ch < 16)
+            {
+                //из верха вних
+                if (ch < 16 - Key)
+                {
+                    return ch + Key + 16;
+                }
+                else
+                {
+                    return ch + Key;
+                }
+            }
+            else
+            {
+                //из вниза верх
+                if (ch >= 16 + Key)
+                {       
+                    return ch - Key - 16;
+                }
+                else
+                {
+                    return ch - Key;
+                }
+            }
+
+        }
+
+        public void method(string text, string keys)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                int offset = char.IsLower(text[i]) ? 1072 : 1040;
+                int ch = text[i] - offset;
+
+                offset = char.IsLower(keys[i % keys.Length]) ? 1072 : 1040;
+                int key = keys[i % keys.Length] - offset;
+                Console.WriteLine((char)(CharacterReplacement(ch, key)+offset));
+            }
+        }
+    }
+}
